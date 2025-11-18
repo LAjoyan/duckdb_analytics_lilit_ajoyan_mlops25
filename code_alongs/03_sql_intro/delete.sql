a2bdbdb9567e1da74f7340c06b31ce44aaacc118
lilit900 <ajoyan.lilit@gmail.com>
2025-11-18 23:57:57 +0100

    created funny_jokes table and inserted data, updated and deleted some jokes

---
+++ code_alongs/03_sql_intro/delete.sql
@@ -0,0 +1,8 @@
+FROM
+    funny_jokes
+WHERE
+    raiting <= 6;
+
+DELETE FROM funny_jokes
+WHERE
+    raiting <= 6;
---
+++ code_alongs/03_sql_intro/queries.sql
@@ -0,0 +1,14 @@
+DESC funny_jokes;
+
+FROM
+    funny_jokes;
+
+FROM
+    funny_jokes
+ORDER BY
+    raiting;
+
+FROM
+    funny_jokes
+ORDER BY
+    raiting DESC;
